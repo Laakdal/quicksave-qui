03acb875a44e15f90f6d4956357aaa668b311669
Laakdal <ario.rafa38@gmail.com>
2026-05-14 14:14:38 +0700

    Update tailwind CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,24 @@ export default {
   theme: {
     extend: {
       colors: {
+        accent: 'rgb(var(--accent) / <alpha-value>)',
+        'accent-hover': 'rgb(var(--accent-hover) / <alpha-value>)',
+        panel: {
+          dark: 'rgb(var(--panel-dark) / <alpha-value>)',
+          darker: 'rgb(var(--panel-darker) / <alpha-value>)',
+        },
         zinc: {
-          850: '#202022',
-          900: '#18181b',
-          950: '#09090b',
+          100: 'rgb(var(--zinc-100) / <alpha-value>)',
+          200: 'rgb(var(--zinc-200) / <alpha-value>)',
+          300: 'rgb(var(--zinc-300) / <alpha-value>)',
+          400: 'rgb(var(--zinc-400) / <alpha-value>)',
+          500: 'rgb(var(--zinc-500) / <alpha-value>)',
+          600: 'rgb(var(--zinc-600) / <alpha-value>)',
+          700: 'rgb(var(--zinc-700) / <alpha-value>)',
+          800: 'rgb(var(--zinc-800) / <alpha-value>)',
+          850: 'rgb(var(--zinc-850) / <alpha-value>)',
+          900: 'rgb(var(--zinc-900) / <alpha-value>)',
+          950: 'rgb(var(--zinc-950) / <alpha-value>)',
         }
       }
     },
